764640aea5fb7720d0f43133ecde1b8d8aff4c67
drew <andre@vollrath.ca>
2017-02-06 20:09:25 -0800

    fleshed out junk_controller

--- app/controllers/junk_controller.rb
+++ app/controllers/junk_controller.rb
@@ -1,8 +1,55 @@
 class JunkController < ApplicationController
   
   get '/junk' do
-    #redirect_if_not_logged_in
+    redirect_if_not_logged_in
     erb :'junk/index'    
   end
 
+  get '/junk/new' do
+    redirect_if_not_logged_in
+    erb :'tweets/new'
+  end
+
+  get '/junk/:id/edit' do
+    redirect_if_not_logged_in
+    @junk = Junk.find(params[:id])
+    
+    if session[:user_id] == @junk.user_id
+      erb :'junk/edit'      
+    else
+      redirect to '/junk'
+    end
+  end
+
+  get '/junk/:id' do
+    redirect_if_not_logged_in
+    @junk = Junk.find(params[:id])  
+    erb :'junk/show'
+  end
+
+  patch '/junk/:id' do
+    if params[:content].empty?
+      redirect to "/junk/#{params[:id]}/edit"
+    else 
+      #find and update junk
+      redirect to "/junk/#{tweet.id}"
+    end
+  end
+
+  post '/junk' do
+    if params[:content].empty? 
+      redirect to "/junk/new"
+    else
+      ##Create Junk
+      redirect to "/junk/#{junk.id}"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    redirect_if_not_logged_in
+    junk = Junk.find(params[:id])
+    
+    junk.delete if session[:user_id] == junk.user_id
+    redirect to '/junk'
+  end
 end
