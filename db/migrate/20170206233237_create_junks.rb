78732b55c078afbaf7e78ff3a96c28729060170a
drew <andre@vollrath.ca>
2017-02-06 16:00:52 -0800

    completed migrations

--- db/migrate/20170206233237_create_junks.rb
+++ db/migrate/20170206233237_create_junks.rb
@@ -1,4 +1,12 @@
 class CreateJunks < ActiveRecord::Migration[5.0]
   def change
+    create_table :junks do |t|
+      t.string :name
+      t.string :img_url
+      t.string :url
+      t.string :note
+      t.integer :user_id
+      t.integer :tag_id
+    end
   end
 end
