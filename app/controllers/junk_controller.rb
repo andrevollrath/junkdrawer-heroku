f9739998b2d0b1be9bd371c977a5705335e180e4
drew <andre@vollrath.ca>
2017-02-07 17:40:38 -0800

    updated views, controllers & layout

--- app/controllers/junk_controller.rb
+++ app/controllers/junk_controller.rb
@@ -26,6 +26,7 @@ class JunkController < ApplicationController
   get '/junk/:id' do
     redirect_if_not_logged_in
     @junk = Junk.find(params[:id])  
+    puts @junk
     erb :'junk/show'
   end
 
@@ -37,13 +38,12 @@ class JunkController < ApplicationController
       redirect to "/junk/#{tweet.id}"
     end
   end
-
+  
   post '/junk' do
     #Check for content requirements
     if Junk.no_params(params)
       redirect to "/junk/new"
     else
-      puts params
       junk = Junk.create(params[:junk])
       junk.tag = Tag.find_or_create_by(name: params[:tag])
       junk.user = User.find(session[:user_id])
